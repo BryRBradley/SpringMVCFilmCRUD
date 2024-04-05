a6677242917ce6167601c3de4f28f2c8531301df
Bryan Bradley <bryrbradley@gmail.com>
2024-04-05 17:27:49 -0600

    commit message

--- src/main/java/com/skilldistillery/film/data/DatabaseAccessorObject.java
+++ src/main/java/com/skilldistillery/film/data/DatabaseAccessorObject.java
@@ -9,10 +9,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
-import com.skilldistillery.film.entities.Actor;
-import com.skilldistillery.film.entities.Film;
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
 
 @Component
 public class DatabaseAccessorObject implements DatabaseAccessor {
